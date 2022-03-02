7e431178a606dbe71bf5fc881e2ab427ad7ec9c8
Ajinkya Sonawane <sonawaneajinks@gmail.com>
2022-03-01 21:39:10 -0500

    Add OpenSearch implementation

---
+++ OpenSearch/README.md
@@ -0,0 +1,32 @@
+### Create Domain
+- Create domain 
+- Provide Domain Name
+- Deployment type - Development & Testing
+- Version - Choose 1.1 (latest)
+- Data Nodes - Instance Type: `t3.small.search`
+- Network - Public Access
+- Fine-grained access policy: `Set IAM ARN as master user`
+- Access Policy: `Only use fine-grained access control`
+- Add Following to resource policy :
+`{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::123456789012:user/<USER_NAME>"
+      },
+      "Action": "es:*",
+      "Resource": "arn:aws:es:us-east-1:123456789012:domain/<DOMAIN_NAME>/*"
+    }
+  ]
+}`
+
+### Requirements 
+- pip install opensearch-py
+- pip install requests_aws4auth
+- pip install pyyaml
+- pip install boto3
+
+### .ENV
+- Store the domain name without the "https://" in OPEN_SEARCH_HOST key
---
+++ OpenSearch/uploadDataToOpeanSearch.py
@@ -0,0 +1,77 @@
+from requests_aws4auth import AWS4Auth
+from opensearchpy import OpenSearch, RequestsHttpConnection
+from decouple import config
+import boto3
+import yaml
+
+
+PATH = "../YelpDataFetcher/"
+CUISINES_LIST = ['Indian', 'Mexican', 'Chinese', 'Korean', 'Japanese']
+index_name = 'restaurant-index'
+region = "us-east-1"
+service = "es"
+creds = boto3.Session().get_credentials()
+awsauth = AWS4Auth(creds.access_key, creds.secret_key, region, service, session_token=creds.token)
+host = config("OPEN_SEARCH_HOST")
+client = OpenSearch(
+        hosts=[{'host': host, 'port': 443}],
+        http_auth=awsauth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection
+    )
+
+
+def create_index():
+    index_body = {
+        'settings': {
+            'index': {
+            }
+        }
+    }
+    response = client.indices.create(index_name, body=index_body)
+
+
+def upload_json_to_index():
+    records = []
+    id = 1
+    for filename in CUISINES_LIST:
+        data = yaml.safe_load(open("%s%s.json" % (PATH, filename)))
+        for record in data:
+            document = {"restaurant_id": record["id"], "cuisine": filename}
+            response = client.index(
+                index='restaurant-index',
+                body=document,
+                id=id,
+                refresh=True
+            )
+            id += 1
+            print(response)
+
+
+def search_data_on_index():
+    # Search for the document.
+    q = 'Indian'
+    query = {
+        'size': 5,
+        'query': {
+            'multi_match': {
+                'query': q,
+                'fields': ['cuisine']
+            }
+        }
+    }
+
+    response = client.search(
+        body=query,
+        index=index_name
+    )
+    print('\nSearch results:')
+    for record in response['hits']['hits']:
+        print(record)
+
+
+if __name__ == "__main__":
+    create_index()
+    upload_json_to_index()
+    search_data_on_index()
